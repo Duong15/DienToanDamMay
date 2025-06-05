1105df58ac53690b10cbeeb7f43156cb0150238c
Duong15 <duong_dpm215435@student.agu.edu.vn>
2025-06-05 08:59:08 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Hiển thị trang đăng nhập
 router.get('/login', (req, res) => {
